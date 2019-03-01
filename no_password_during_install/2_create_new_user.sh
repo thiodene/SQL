00ebe18090a1034601e224b7dea26cea109fec77
Serge Ayissi <ayissi.serge@gmail.com>
2019-03-01 12:40:22 -0500

    Create 2_create_new_user.sh

---
+++ no_password_during_install/2_create_new_user.sh
@@ -0,0 +1,12 @@
+# Now create a new user to avoid issues with ROOT user (connecting too many times will get you root banned!)
+# Creating users and databases
+
+# To create a MySQL database and user, follow these steps:
+# At the command line, log in to MySQL as the root user: 
+mysql -u root -p
+
+# To create a database user, type the following command. Replace username with the user you want to create, 
+# and replace password with the user's password:
+GRANT ALL PRIVILEGES ON *.* TO 'username'@'localhost' IDENTIFIED BY 'password';
+
+# Now you should be able to connect with that new user on PHPMYADMIN
