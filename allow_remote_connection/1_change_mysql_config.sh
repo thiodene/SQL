9e96f27e05104dc7d2e0e9af4b05108159b4907a
Serge Ayissi <ayissi.serge@gmail.com>
2019-03-15 16:56:46 -0400

    Create 1_change_mysql_config.sh

---
+++ allow_remote_connection/1_change_mysql_config.sh
@@ -0,0 +1,13 @@
+# Version 5.7
+# Go to MySQL config and change the bind-address from 127.0.0.1 to 0.0.0.0
+sudo vi /etc/mysql/mysql.conf.d/mysqld.cnf
+# -> bind-address = 0.0.0.0
+
+# Now Save the file and restart MySQL
+[ESC] + ZZ
+
+#restart MySQL
+sudo service mysql restart
+
+# Verify the changes
+View Before and After picture
