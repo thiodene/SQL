e6ad03fb710364511b5fb46a5176ed3ef2560a8f
Serge Ayissi <ayissi.serge@gmail.com>
2019-03-28 16:50:46 -0400

    Create restart_ubuntu.sh

---
+++ restart_mysql/restart_ubuntu.sh
@@ -0,0 +1,18 @@
+# Debian/Ubuntu Linux users
+
+# The syntax is as follows to start/stop/restart mysql server:
+
+#### To start ####
+/etc/init.d/mysql start
+# OR
+service mysql start
+ 
+#### To stop ####
+/etc/init.d/mysql stop
+# OR
+service mysql stop
+ 
+#### To restart ####
+/etc/init.d/mysql restart
+# OR
+service mysql restart
