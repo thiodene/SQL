9095c583c0176168aa27b0f92096f9d86bb03feb
Serge Ayissi <ayissi.serge@gmail.com>
2019-03-28 16:51:59 -0400

    Create restart_centos.sh

---
+++ restart_mysql/restart_centos.sh
@@ -0,0 +1,18 @@
+# CentOS/RHEL/Fedora/Scientific Linux version 4.x/5.x/6.x or older users
+
+# The syntax is:
+
+### To start ###
+service mysqld start
+# OR
+/etc/init.d/mysqld start
+ 
+### To stop ###
+service mysqld stop
+# OR
+/etc/init.d/mysqld stop
+ 
+### To restart ###
+service mysqld restart
+# OR
+/etc/init.d/mysqld restart
