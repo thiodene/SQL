bad7391c5b3f62a7f6ba02411450858cb38fbe1d
Serge Ayissi <ayissi.serge@gmail.com>
2019-04-20 20:40:50 -0400

    Update move_files_to_mysql_folder.sh

--- import_large_csv/move_files_to_mysql_folder.sh
+++ import_large_csv/move_files_to_mysql_folder.sh
@@ -1,3 +1,4 @@
-For the files to be used into the upload from CSV to SQL format put them in the MySQL folder
+#For the files to be used into the upload from CSV to SQL format put them in the MySQL folder
+# right folder on Ubuntu
 sudo cp ipv6.csv /var/lib/mysql-files/.
 sudo cp ipv4.csv /var/lib/mysql-files/.
